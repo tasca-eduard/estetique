e7680d5418543aa1d05db5d87bd3dd612d3db782
Eduard T <eduard.t@wavemyth.com>
2023-02-14 13:45:55 +0200

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import Slide from "./components/Slider/Slide";
+
+const slides = [
+  {
+    fontFamily: "Montserrat",
+    bgColor: "#412f2d",
+    textColor: "#f99195",
+    title: "Montserrat",
+  },
+  {
+    fontFamily: "Playfair Display",
+    bgColor: "#f3e3d6",
+    textColor: "#282222",
+    title: "Playfair Display",
+  },
+  {
+    fontFamily: "Abril Fatface",
+    bgColor: "#18281b",
+    textColor: "#efe5cf",
+    title: "Abril Fatface",
+  },
+  {
+    fontFamily: "Rochester",
+    bgColor: "#f7f7f9",
+    textColor: "#091442",
+    title: "Rochester",
+  }
+]
+
+function App() {
+  return (
+    <div className="slider">
+      {slides.map((slide, index) => {
+        return (
+          <Slide 
+            key={slide.title}
+            {...slide}
+            index={index + 1}
+          />
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Slider/Slide.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+interface Props {
+  fontFamily: string,
+  bgColor: string,
+  textColor: string,
+  title: string,
+  index: number
+}
+
+export default function Slide({
+  fontFamily,
+  bgColor,
+  textColor,
+  title,
+  index,
+}: Props) {
+  const SENTENCE = "The quick brown fox jumps over the lazy dog.";
+  const ALPHABETH = "abcdefghijklmnopqrstuvwxyz";
+  const DIGITS = 1234567890;
+  const formattedIndex = index < 9 ? `0${index}` : index;
+  const style = {
+    "--background": bgColor,
+    "--color": textColor,
+    "--fontFamily": fontFamily,
+    "--circleBg": textColor
+  } as React.CSSProperties;
+
+  return (
+    <div
+      className="slide"
+      style={style}
+    >
+      <div className="container">
+        <div className="flex-row between">
+          <div className="column">#UITIPS</div>
+          <div className="column">{formattedIndex}</div>
+        </div>
+        <div className="flex-row">
+          <h2>{title}</h2>
+        </div>
+        <div className="flex-row">
+          <h3>{SENTENCE}</h3>
+        </div>
+        <div className="flex-row">
+          <h4>{ALPHABETH.toUpperCase()}</h4>
+        </div>
+        <div className="flex-row">
+          <h4>{ALPHABETH.toLowerCase()}</h4>
+        </div>
+        <div className="flex-row">
+          <h4>{DIGITS}</h4>
+        </div>
+        <div className="flex-row">
+          <div className="circle"></div>
+          <span>{textColor}</span>
+        </div>
+        <div
+          className="flex-row"
+          style={{
+            "--circleBg": bgColor
+          } as React.CSSProperties}
+        >
+          <div className="circle"></div>
+          <span>{bgColor}</span>
+        </div>
+        <div className="flex-row between">
+          <small className="column">CTRL + LEFT / RIGHT ARROW</small>
+          <small className="column">or</small>
+          <small className="column">SHIFT + MOUSE WHEEL</small>
+        </div>
+      </div>
+    </div>
+  )
+}
